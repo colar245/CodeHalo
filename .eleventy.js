127b15f7bdcb4b70af6ddfa48fc9f069a4cce483
Mladenovic Lazar <lazar.mladenovic245@gmail.com>
2021-02-10 19:55:18 +0100

    robot.txt and sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function (config) {
 
   config.addPassthroughCopy("src/assets");
+  config.addPassthroughCopy("src/sitemap.xml");
+  config.addPassthroughCopy("src/robot.txt");
   return {
     dir: {
       input: "src",

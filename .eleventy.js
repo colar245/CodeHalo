830de2bcbf127c754bffe8e73e82273b8727e1cd
Mladenovic Lazar <lazar.mladenovic245@gmail.com>
2021-02-10 22:48:11 +0100

    Netlify redirects

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function (config) {
   config.addPassthroughCopy("src/assets");
   config.addPassthroughCopy("src/sitemap.xml");
   config.addPassthroughCopy("src/robots.txt");
+  config.addPassthroughCopy("_redirects");
   return {
     dir: {
       input: "src",

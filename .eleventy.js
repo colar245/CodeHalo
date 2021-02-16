f70347d5db0b80be3d2ec3d5c6782a335afeefef
Mladenovic Lazar <lazar.mladenovic245@gmail.com>
2021-02-16 10:57:27 +0100

    google site verification

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function (config) {
   config.addPassthroughCopy("src/assets");
   config.addPassthroughCopy("src/sitemap.xml");
   config.addPassthroughCopy("src/robots.txt");
+  config.addPassthroughCopy("googled41287c90f49992b.html");
   config.addPassthroughCopy("_redirects");
   return {
     dir: {

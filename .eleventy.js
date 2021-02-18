aed2eca66287678849b7209ccdcabe01549efd95
Mladenovic Lazar <lazar.mladenovic245@gmail.com>
2021-02-18 17:54:51 +0100

    Security Headers

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,6 @@ module.exports = function (config) {
   config.addPassthroughCopy("src/sitemap.xml");
   config.addPassthroughCopy("src/robots.txt");
   config.addPassthroughCopy("googled41287c90f49992b.html");
-  config.addPassthroughCopy("_redirects");
   return {
     dir: {
       input: "src",
